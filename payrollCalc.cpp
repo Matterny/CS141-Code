bb72947ed65d49235f1981bee71699ea622df45f
Matthew Rodman <matterny@ymail.com>
2018-04-22 23:06:43 -0400

    Most of the payrollcalc is done, a few bugs

--- payrollCalc.cpp
+++ payrollCalc.cpp
@@ -1,17 +1,21 @@
 #include <sstream>
-#include <string>
-#include <iostream>
-#include <fstream>
-using namespace std;
-#define read(inFile);    inFile>>Employees[x].wage.hourly>>Employees[x].name.Fn>>Employees[x].name.Mn>>Employees[x].name.Ln>>Employees[x].department>>Employees[x].manager.Fn>>Employees[x].manager.Mn>>Employees[x].manager.Ln>>starttime >> lunchstart >> lunchend >> endtime;
-//This is because these lines are so long I dont want them to exist inside my code.
-float timeToInt(std::string);
-void readData(string);
+  #include <string>
+  #include <iostream>
+  #include <fstream>
+  using namespace std;
+    float timeToInt(std::string);
+    void readData(string);
+    float hoursWorked(string ,string , string , string );
+    float breakLength(string clockIN,string lunchStart, string lunchEnd, string clockout);
+    float overtime(string clockIN,string lunchStart, string lunchEnd, string clockout);
+    float totalPay(float totalHrs,float otHours,float wage, float otPay, char otEle);
+    void writeData(string);
+    enum days {monday, tuesday, wednesday, thursday, friday};
+    #define readf     inFile>>Employees[x].wage.hourly>>Employees[x].wage.otEle>>Employees[x].wage.otRate>>Employees[x].name.Fn>>Employees[x].name.Mn>>Employees[x].name.Ln>>Employees[x].department>>Employees[x].manager.Fn>>Employees[x].manager.Mn>>Employees[x].manager.Ln>>starttime >> lunchstart >> lunchend >> endtime;
 
-enum days {monday, tuesday, wednesday, thursday, friday};
 struct Wage{
   float hourly, otRate;
-  bool otEle;
+  char otEle;
 };
 struct Person{
   string Fn, Mn, Ln;
@@ -28,17 +32,13 @@ struct employee{
 employee Employees[20];
 
 int main(){
-  std::string timeInString = "11:15";
-  ifstream inputFile;
-  inputFile.open("payrollData.txt");
-  int i=0;
-  while(!inputFile.eof()){
-  char x;
-  i++;
-  cout<<i<<"\n";
-  inputFile>>x;
-  }
-  timeToInt(timeInString);
+  cout<<"File to open: \n";
+  string nameOfFile;
+  cin>>nameOfFile;
+  readData(nameOfFile);
+  cout<<"\nFile to write: \n";
+  cin>>nameOfFile;
+  writeData(nameOfFile);
   return 0;
 }
 
@@ -56,25 +56,93 @@ float timeToInt(std::string timeInString){
   std::istringstream(timeMins) >> timeMinsInt;
   return timeHoursInt+timeMinsInt/60;
 }
-
 float hoursWorked(string clockIN,string lunchStart, string lunchEnd, string clockout){
   return -timeToInt(clockIN)+timeToInt(clockout)-timeToInt(lunchStart)+timeToInt(lunchEnd);
+}
+float breakLength(string clockIN,string lunchStart, string lunchEnd, string clockout){
+  return -timeToInt(lunchStart)+timeToInt(lunchEnd);
+}
+float overtime(string clockIN,string lunchStart, string lunchEnd, string clockout){
+  if(-timeToInt(clockIN)+timeToInt(clockout)-timeToInt(lunchStart)+timeToInt(lunchEnd)>0)
+  return 40-timeToInt(clockIN)+timeToInt(clockout)-timeToInt(lunchStart)+timeToInt(lunchEnd);
+  return 0;
+}
+float totalPay(float totalHrs,float otHours,float wage, float otPay, char otEle){
+  if(otEle == 'Y')
+    return totalHrs*wage+otHours*(otPay-1)*wage;
+  else if (otEle == 'N')
+    return totalHrs*wage;
+  else {
+    cout <<"\nDB ERROR, PLEASE RECOMPUTE AFTER FIXING\n";
+    return -1;
+  }
 }
 
 void readData(string fileName){
   ifstream inFile;
   inFile.open(fileName.c_str());
   int x=0;
-  while(!inFile.eof()){
-    string starttime,lunchstart, lunchend, endtime;
-    read(inFile);
-    for(int count=0;count<5;count++){
-      read(inFile);
-      Employees[x].timecard[count].hrsWorked = hoursWorked(starttime,lunchstart,lunchend,endtime);
-    }
+  string endtime;
+  while(x<20){
+    string starttime,lunchstart, lunchend;
 
+    for(int count=0;count<5;count++){
+      readf
+      Employees[x].timecard[count].hrsWorked = hoursWorked(starttime,lunchstart,lunchend,endtime);
+      Employees[x].timecard[count].lunchBreakLength = breakLength(starttime,lunchstart,lunchend,endtime);
+      Employees[x].timecard[count].otHours = overtime(starttime,lunchstart,lunchend,endtime);
+      Employees[x].timecard[count].totalPay = totalPay(Employees[x].timecard[count].hrsWorked,
+        Employees[x].timecard[count].otHours,Employees[x].wage.hourly,Employees[x].wage.otRate,Employees[x].wage.otEle);
+      }
     x++;
   }
-
+  cout <<endl<<endtime<<endl;
   inFile.close();
+}
+
+void writeData(string fileName){
+  if (fileName.substr(max(4, fileName.size())-4) != string(".xml")) {
+    cout<<"Bad output fileName\n";
+    return;
+  }
+  ofstream outfile;
+  outfile.open(fileName.c_str());
+  int x=0;
+  while(x<20){
+  outfile<<"<payroll>\n<employee>\n
+ <name>"<< Employees[x].name.Fn<<" "<<Employees[x].name.Mn<<" "<<Employees[x].name.Ln<<"</name>\n
+ <manager>"<< Employees[x].manager.Fn<<" "<<Employees[x].manager.Mn<<" "<<Employees[x].manager.Ln<<"</name>\n
+ <department>"<<Employees[x].department<<"</department>\n
+ <payrate>"<<Employees[x].wage.hourly<<"</payrate>\n
+ <overtime>";
+ if(Employees[x].wage.otEle=='Y')
+  outfile<<"Yes";
+ else if(Employees[x].wage.otEle=='N')
+  outfile<<"No"
+ else outfile<<"error";
+
+ outfile<<"</overtime>\n
+ <multiplier>1.3</multiplier>\n
+ <hours>45</hours>\n
+ <overhours>7.5</overhours>\n
+ <lunch>6</lunch>\n
+ <pay>718.20</pay>\n
+ </employee>\n
+ <employee>\n
+ <name>Jessica Alice Waters</name>\n
+ <manager>Jane Mary Hargreave</name>\n
+ <department>Avionics</department>\n
+ <payrate>19.40</payrate>\n
+ <overtime>No</overtime>\n
+ <multiplier>0</multiplier>\n
+ <hours>37.5</hours>\n
+ <overhours>0</overhours>\n
+ <lunch>5</lunch>\n
+ <pay>727.50</pay>\n
+ </employee>\n
+</payroll>";
+x++;
+}
+
+  outfile.close();
 }
