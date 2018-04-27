eab3445be3bd70af61e3648046f37e28f42b3ba8
Matthew Rodman <matterny@ymail.com>
2018-04-27 13:15:39 -0400

    Lab Final and FinalProject directory. Some hangman text files complete.

--- FinalProject/c++Final.cpp
+++ FinalProject/c++Final.cpp
@@ -7,9 +7,10 @@
 //Global functions
 void drawMan(int);
 void selectOption();
-
-//Global Macros. Not 100% needed, but I'm not willing to go without for this. 
-#define FileSize 50
+bool guessInWord(char);
+void guessingGame();
+//Global Macros. Not 100% needed, but I'm not willing to go without for this.
+#define FileSize 7    //Can be made larger, IF the files are all increased in size by 1 line.
 #define cinToUpper    cin>>option;for(int i=0;i<option.length();i++)option[i]=toupper(option[i])
 //global Variables
 struct word{
@@ -22,12 +23,19 @@ struct word{
 word GuessingOption;
 
 int main(){
+  while(1){
   selectOption();
-  drawMan(4);
-}
+  guessingGame();
 
-void drawMan(int stage){
-  //Draw the man hanging, with 6 being perfect and 0 being dead.
+  cout<<"Play again?\n";
+  char answer;
+  cin>>answer;
+  if(toupper(answer)!='Y'){
+    break;
+  }
+}}
+
+void drawMan(int stage){//Draw the man hanging, with 6 being perfect and 0 being dead.
   switch (stage) {
     case 0:
         cout
@@ -142,4 +150,30 @@ void selectOption(){
   inputFile >>GuessingOption.word>>GuessingOption.hint1>>GuessingOption.hint2;
 
   inputFile.close();
+}
+
+bool guessInWord(char guess){
+  if(GuessingOption.word.find(guess) != std::string::npos)return 1;
+  return 0;
+}
+void guessingGame(){
+  int badTries = 6;
+  string prevTries = "";
+  while (badTries>0){
+    system("clear");
+    cout<<"Past tries: "<<prevTries<<endl;
+    cout<<"The catagory is inside the file: " << GuessingOption.catagory<<endl;
+    if(badTries<3)cout<<"First hint: "<<GuessingOption.hint1<<endl;
+    if(badTries<2)cout<<"Last hint: "<<GuessingOption.hint2<<endl;
+    cout<<"Please guess a letter: \n";
+    drawMan(badTries);
+    char guess;
+    cin>>guess;
+    if(guessInWord(guess)){
+      prevTries = prevTries+guess;
+    }
+    else{
+      badTries--;
+    }
+  }
 }
---
+++ libraryManagement.cpp
@@ -0,0 +1,79 @@
+#include<fstream>
+  #include<iostream>
+  #include<cstdlib>
+  #include<string>
+  using namespace std;
+
+struct library{
+  string title;
+  char in;
+  string Fn, Ln;
+  int late;
+};
+library libraryData[20];
+
+void readFile(){
+  ifstream inputs;
+  inputs.open("library.txt");
+  for(int x=0;x<20;x++){
+    inputs>>libraryData[x].title>>libraryData[x].in>>libraryData[x].Fn>>libraryData[x].Ln>>libraryData[x].late;
+  }
+  inputs.close();
+}
+void viewAll(){
+  for(int x=0;x<20;x++){
+    cout<<libraryData[x].title<<" "<<libraryData[x].in<<" "<<libraryData[x].Fn<<libraryData[x].Ln<<libraryData[x].late<<endl;
+  }
+}
+void showbypatron(string fn, string ln){
+int hasAny = 0;
+  for(int x=0;x<20;x++){
+    if ((libraryData[x].Fn==fn)){//cout<<"FN";
+    if(libraryData[x].Ln==ln){//cout<<"LN";
+      hasAny++;
+      cout<<libraryData[x].title<<" "<<libraryData[x].late<<endl;
+    }}
+  }
+  if(hasAny)cout<<fn<<" "<<ln<<" has "<<hasAny<<" Books checked out right now\n";
+  else cout<<"This user has no books signed out at this time\n";
+}
+void calcPenalty(){
+  for(int x=0;x<20;x++){
+    int penalty = libraryData[x].late;
+    if ((libraryData[x].late!=-1))
+    float payment = .1*penalty+(penalty>6)*(.1*penalty)+(penalty>10)*penalty*.05;
+    cout<<libraryData[x].Fn<<" "<<libraryData[x].Ln<<" "<<libraryData[x].title<<" $"<<payment<<endl;
+  }
+}
+
+void ShowMenu(){
+  //while(1)
+  {
+    system("clear");
+    cout<<"Please select an option\n1. Show all books loaned out by a patron\n2. List all overdue books and their penalty\n3. Quit the program\nPlease select a number: ";
+    char option;
+    cin>>option;
+    if (option=='1'){
+      cout<<"Please enter the first and last name, seperated by a space: ";
+      string fn, ln;
+      cin>>fn>> ln;
+      showbypatron(fn,ln);
+      //Show all books loaned out by patron
+    }
+    else if(option == '2'){
+      //List all overdue books and penalty
+      calcPenalty();
+    }
+    else if (option =='3'){
+      //break;
+      cout<<"\nThank you for using the libraTron 100";
+    }
+  }
+}
+
+int main(){
+  readFile();
+  //viewAll();
+  ShowMenu();
+  return 0;
+}
